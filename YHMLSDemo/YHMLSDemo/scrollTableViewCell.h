bd36875c59218a232a902f943aedb63b6fb0c037
name <emailaddress>
2017-02-13 14:36:20 +0800

    no message

---
+++ YHMLSDemo/YHMLSDemo/scrollTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  scrollTableViewCell.h
+//  YHMLSDemo
+//
+//  Created by 我叫MT on 17/2/13.
+//  Copyright © 2017年 Pinksnow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface scrollTableViewCell : UITableViewCell
+
+@end
---
+++ YHMLSDemo/YHMLSDemo/scrollTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  scrollTableViewCell.m
+//  YHMLSDemo
+//
+//  Created by 我叫MT on 17/2/13.
+//  Copyright © 2017年 Pinksnow. All rights reserved.
+//
+
+#import "scrollTableViewCell.h"
+
+@implementation scrollTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
