dc6219abcd90e6f28e4c4eb21fc6279b914c2cc5
name <emailaddress>
2017-02-16 17:53:22 +0800

    no message

--- YHMLSDemo/YHMLSDemo/GunDongTableViewCell.m
+++ YHMLSDemo/YHMLSDemo/GunDongTableViewCell.m
@@ -9,11 +9,13 @@
 #import "GunDongTableViewCell.h"
 #import <QuartzCore/QuartzCore.h>
 #import "PagedFlowView.h"
-@interface GunDongTableViewCell ()
+@interface GunDongTableViewCell ()<PagedFlowViewDelegate,PagedFlowViewDataSource>
 @property(nonatomic, strong)UILabel *titleLabel;
 @property(nonatomic, strong)UIImageView *titleImage;
-@property(nonatomic, strong)UIScrollView *scrollView;
+@property(nonatomic, strong)NSMutableArray *imageArray;
 @property (nonatomic, strong) PagedFlowView *hFlowView;
+
+
 @end
 @implementation GunDongTableViewCell
 
@@ -26,7 +28,7 @@
 {
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
     if (self) {
-        
+        [self creatView];
     }
     return self;
 }
@@ -35,6 +37,28 @@
 {
     self.titleLabel = [[UILabel alloc]init];
     [self.contentView addSubview:self.titleLabel];
+    self.titleImage = [[UIImageView alloc]init];
+    [self.contentView addSubview:self.titleImage];
+    self.hFlowView = [[PagedFlowView alloc]init];
+    [self.contentView addSubview:self.hFlowView];
+    self.imageArray = [NSMutableArray arrayWithObjects:@"",@"",@"",@"",@"",nil];
+    [self.titleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.mas_equalTo(-5);
+        make.top.mas_equalTo(10);
+    }];
+    weak(weakSelf);
+    [self.titleImage mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.mas_equalTo(weakSelf.titleLabel.mas_right).offset(5);
+        make.centerY.mas_equalTo(weakSelf.titleLabel.mas_centerY);
+        make.width.height.mas_equalTo(15);
+    }];
+    [self.hFlowView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.mas_equalTo(0);
+        make.top.mas_equalTo(weakSelf.titleLabel.mas_bottom).offset(15);
+        make.left.mas_equalTo(10);
+        make.right.mas_equalTo(10);
+    }];
+    
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
